fbf99c10b578e448ce95d12a6af9ff4f7e7dfb67
Mary Claire Capili <maryclaireosongco@gmail.com>
2023-04-06 00:57:14 +0800

    Activity1

---
+++ src/main/kotlin/KotlinActivity1.kt
@@ -0,0 +1,60 @@
+fun main(){
+    println("Application For Employment")
+    println("")
+    val job:String = "Mobile Developer"
+    val salary:Double = 40000.00
+    val dateAvailable:String = "Full Time"
+    val lastName:String = "Capili"
+    val firstName:String = "Mary Claire"
+    val middleName:String = "Osongco"
+    val address:String = "Mabini St. Brgy. Tambak (POB6)"
+    val city:String = "Pagbilao"
+    val state:String = "Quezon"
+    val zip:Int = 4302
+    val homePhone:Int = 42-7191792
+    val cellPhone:Long = 9928310994
+    val email:String = "maryclaireosongco@gmail.com"
+    val sss:Long = 123456789
+    val usCitizen:String = "No"
+    val convicted:String = "No"
+    val preDrugTest:String = "Yes"
+    val school:String = "Manuel S. Enverga University Foundation"
+    val location:String = "Lucena City"
+    val yearAttended:String = "Year 2005-2009"
+    val degree:String = "Bachelor of Science"
+    val major:String = "Hotel and Restaurant Management"
+    val otherCerti:String = "N/A"
+
+    println("Position You Are Applying For is $job")
+    println("Desired Salary : $salary")
+    println("Date Available for Work : $dateAvailable")
+    println("")
+    println("PERSONAL INFORMATION")
+    println("")
+    println("Last Name : $lastName")
+    println("First Name : $firstName")
+    println("Middle Name : $middleName")
+    println("Address : $address")
+    println("City : $city")
+    println("State : $state")
+    println("Zip : $zip")
+    println("Home Phone : $homePhone")
+    println("Cell Phone : $cellPhone")
+    println("Email Address : $email")
+    println("Social Security Number : $sss")
+    println("Are you a U.S. Citizen? $usCitizen")
+    println("Have you ever convicted of a felony? $convicted")
+    println("If selected for employment are you willing to submit to a pre-employment drug screening test? $preDrugTest")
+    println("")
+    println("EDUCATION")
+    println("")
+    println("School Name : $school")
+    println("Location : $location")
+    println("Years Attended : $yearAttended")
+    println("Degree Received : $degree")
+    println("Major : $major")
+    println("Other training, certifications or licenses held: $otherCerti")
+    println("")
+    println("EMPLOYMENT")
+    println("")
+}
