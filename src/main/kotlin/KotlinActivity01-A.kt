d0880245808f646a59df4f23218656538e9ced91
Mary Claire Capili <maryclaireosongco@gmail.com>
2023-04-13 03:54:39 +0800

    Activity01-A

---
+++ src/main/kotlin/KotlinActivity01-A.kt
@@ -0,0 +1,42 @@
+fun main(){
+    //Student's Directory
+    var studentIdNumber:Int = 366919
+    var course:String = "Mobile Development"
+    var studentName:String = "Mary Claire B. Osongco"
+    var studentNickname:String = "Claire"
+    var studentAddress:String = "No. 7 Mabini St., Brgy. Tambak (POB6), Pagbilao, Quezon Province"
+    var civilStat:String = "Single"
+    var sex:String = "Female"
+    var birthDate:String = "November 9, 1998"
+    var birthPlace:String = "Lucena City"
+    var nationality:String = "Filipino"
+    var religion:String = "Roman Catholic"
+    var telNumber:Int = 427191792
+    var cpNumber:Long = 9928310994
+    var email:String = "maryclaireosongco@gmail.com"
+    var fathersName:String = "Eduardo R. Osongco"
+    var fathersOccupation:String = "Retired Police"
+    var mothersName:String = "Ma. Corazon B. Osongco"
+    var mothersOccupation:String = "Clinical Instructor"
+    var birthOrder:String = "Eldest"
+    var spousesName:String = "N/A"
+    var spousesOccupation:String = "N/A"
+    var spousesEduAttainment:String = "N/A"
+    var spouseAge:Int = 0
+    var noOfDependetns:Int = 0
+    var siblingsName:String = "Eduardo B. Osongco Jr."
+    var siblingAge:Int = 33
+    var siblingCivilStat:String = "Married"
+    var schoolOrCampany:String = "Quezon Memorial Center"
+    //For Academic Progress Report
+    var classAttendance:String = "Excellent"
+    var classParticipation:String = "Above Average"
+    var classAssignments:String = "Above Average"
+    var exams:String = "Above Average"
+    var studentsGradeToDate:String = ""
+    var howMayThisStudentImproveGrade:String = ""
+    var instructorsComment:String = ""
+
+
+}
+
