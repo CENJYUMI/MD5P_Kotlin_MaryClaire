bbbba8104741866d5fd43a0dc828bce62008e985
Mary Claire Capili <maryclaireosongco@gmail.com>
2023-04-12 21:05:56 +0800

    KotlinProgram6.kt

---
+++ src/main/kotlin/KotlinProgram6.kt
@@ -0,0 +1,12 @@
+fun main(){
+    // logical operators
+    var andOperator = (6 > 1) && (5 > 2) // will only be tru if both statement are true
+    var orOperator = (1 > 5) || (6 > 3) // will only be false if both statements are false
+    var notOperator = !(5 == 5) // inverts the result
+
+    println(andOperator)
+    println(orOperator)
+    println(notOperator)
+
+
+}
