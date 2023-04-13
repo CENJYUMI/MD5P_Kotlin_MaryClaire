bbf7ea17d65ae51f4b3d50c4b1abbe0dbb477dc3
Mary Claire Capili <maryclaireosongco@gmail.com>
2023-04-14 00:51:07 +0800

    PracticeActivity2

---
+++ src/main/kotlin/PracticeActivity2.kt
@@ -0,0 +1,21 @@
+fun main(){
+    //declare all variables
+    var length:Double = 12.5
+    var width:Double = 36.55
+
+    //input
+    println ("Enter the Length")
+    length = readln().toDouble()
+    println ("Enter the Width")
+    width = readln().toDouble()
+
+    //process
+    var perimeter = (length*2) + (width*2)
+    var area = length * width
+
+    //output
+    println("The Perimeter is $perimeter meters")
+    println("")
+    print("The Area is $area meters")
+    println("")
+}
