2d36d65276f625692d616432ce80c2ccacf61934
Mary Claire Capili <maryclaireosongco@gmail.com>
2023-04-15 00:56:17 +0800

    KotlinActivity01-c

---
+++ src/main/kotlin/KotlinActivity01-C.kt
@@ -0,0 +1,79 @@
+fun main(){
+    //Grocery Inventory List
+
+    //(Column Table) inventory id, name, description, Beginning Inventory, Ending Inventory, Sold Items, unit price, Total Sales
+
+    //Fruits
+    //Inventory ID
+    var appleInventoryId1:Int = 10000
+    var appleInventoryId2:Int = 10001
+    var appleInventoryId3:Int = 10002
+
+    //Name
+    var appleName1:String = "Fuji Apple"
+    var appleName2:String = "Red Delicious Apple"
+    var appleName3:String = "Granny Smith Apple"
+
+    //Description
+    var appleDescription:String = "Fruits"
+
+    //Beginning Inventory - This data is base from last inventory.
+    var fujiAppleBeginningInventory:Int = 200 //pieces
+    var redDeliciousAppleBeginningInventory:Int = 200 //pieces
+    var grannySmithAppleBeginningInventory:Int = 200 //pieces
+
+    //Ending Inventory - The data should input by the inventory staff.
+    var fujiAppleEndingInventory:Int = 100 //pieces
+    var redDeliciousAppleEndingInventory:Int = 100 //pieces
+    var grannySmithAppleEndingInventory:Int = 100 //pieces
+
+    //Sold Items  - can be formatted to the difference of beginning and ending inventory.
+    var fujiAppleSold:Int = 100 //pieces
+    var redDeliciousAppleSold:Int = 100 //pieces
+    var grannySmithAppleSold:Int = 100 //pieces
+
+    //Unit Price
+    var fujiAppleSRP:Double = 25.00 //per piece
+    var redDeliciousAppleSRP:Double = 26.00 //per piece
+    var grannySmithAppleSRP:Double = 27.00 //per piece
+
+    //Total Sales - quotient of Unit Price and Sold Items
+
+
+    //Vegetables
+    //Inventory ID
+    var saladGreensInventoryID:Int = 10003
+    var broccoliInventoryID:Int = 10004
+    var carrotInventoryID:Int = 10005
+    var cucumberInventoryID:Int = 10006
+    var garlicInventoryID:Int = 10007
+    var lettuceInventoryID:Int = 10008
+    var tomatoesInventoryID:Int = 10009
+    var mushroomsInventoryID:Int = 10010
+    var onionsInventoryID:Int = 10011
+    var peppersInventoryID:Int = 10012
+    var potatoesInventoryID:Int = 10013
+
+    //name
+    var saladGreens:String = "saladGreens"
+    var broccoli:String = "broccoli"
+    var carrot:String = "carrot"
+    var cucumber:String = "cucumber"
+    var garlicString = "garlic"
+    var lettuce:String = "lettuce"
+    var tomatoes:String = "tomatoes"
+    var mushrooms:String = "mushrooms"
+    var onions:String = "onions"
+    var peppers:String = "peppers"
+    var potatoes:String = "potatoes"
+
+    //Description
+    var vegetables:String = "Vegetables"
+
+
+
+
+
+
+
+}
