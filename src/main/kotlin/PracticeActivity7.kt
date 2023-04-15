49ad10e6732b943d22267a0de02b135939dc2d67
Mary Claire Capili <maryclaireosongco@gmail.com>
2023-04-15 18:56:47 +0800

    Activity 7

---
+++ src/main/kotlin/PracticeActivity7.kt
@@ -0,0 +1,16 @@
+fun main(){
+    //Activity 7
+
+    //Input
+    var num : Int = 0
+
+    //Output
+    println("Enter a Number : ")
+    num = readln().toInt()
+    if(num%2==0){
+        println("The number is even")
+    }else {
+        println("The number is odd")
+    }
+
+}
