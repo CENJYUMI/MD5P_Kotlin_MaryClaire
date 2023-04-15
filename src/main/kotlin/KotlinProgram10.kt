3cc27a2f82566e68c2ce29ca4ecb5bed405fd125
Mary Claire Capili <maryclaireosongco@gmail.com>
2023-04-15 18:54:50 +0800

    Kotlin Program 10

---
+++ src/main/kotlin/KotlinProgram10.kt
@@ -0,0 +1,16 @@
+fun main(){
+    var age : Int = 0
+    // < > <= >= == !=
+
+    println("Please Enter your age: ")
+    age = readln().toInt()
+
+    if(age >= 18){
+        println("Your age is $age")
+        println("You are qualified to vote!")
+    }else{
+        println("Your age is $age")
+        println("Not qualified to vote!!!")
+    }
+    println("This will always display")
+}
