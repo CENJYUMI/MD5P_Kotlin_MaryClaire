766789cc5575ab14d087ce7359857dfaad68ee13
Mary Claire Capili <maryclaireosongco@gmail.com>
2023-04-19 03:11:46 +0800

    Activity 17

---
+++ src/main/kotlin/PracticeActivity17.kt
@@ -0,0 +1,20 @@
+fun main(){
+    var counter:Int = 0
+    var counter1:Int = 0
+    var number:Double = 0.00
+    var average:Double = 0.00
+
+    while(counter < 5){
+        counter1++
+        println("Enter Number $counter1 :")
+        number = readln().toDouble()
+
+        if(counter<6){
+            average +=number/5
+        }
+
+        counter++
+    }
+    println("The Average is ${String.format("%,.2f", average)}")
+
+}
