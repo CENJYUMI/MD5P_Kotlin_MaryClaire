3fc87c1cca33c5b28c76a60764f19e83653fc79f
Mary Claire Capili <maryclaireosongco@gmail.com>
2023-04-19 03:12:44 +0800

    Kotlin Program 15

---
+++ src/main/kotlin/KotlinProgram15.kt
@@ -0,0 +1,15 @@
+fun main(){
+    //while loop
+    //loop var
+    var counter:Int = 0
+    //condition
+    while(counter < 12){
+        println("Claire")
+        //body...
+
+        //update
+        counter++
+
+    }
+    println("Hello World!")
+}
