cc2adbed298416baf62fe0477438c851b6d3388b
Mary Claire Capili <maryclaireosongco@gmail.com>
2023-04-28 02:09:18 +0800

    Kotlin Program 18

---
+++ src/main/kotlin/KotlinProgram18.kt
@@ -0,0 +1,10 @@
+fun main() {
+    //for loop - no need var
+    for(counter in 5..10){
+        if(counter == 7){
+            continue // skip iteration
+        }
+        println(counter)
+    }
+    println("Hello World!")
+}
