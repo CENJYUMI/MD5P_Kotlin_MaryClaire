4e7c738d577985de4ced471bd32340c8116b657a
Mary Claire Capili <maryclaireosongco@gmail.com>
2023-04-28 02:09:58 +0800

    Kotlin Program 21

---
+++ src/main/kotlin/KotlinProgram21.kt
@@ -0,0 +1,13 @@
+fun main(){
+    //only string
+    var names = arrayOf<String>("paul", "john", "ringo", "george")
+    //any data type
+    //var names = arrayOf("paul", "john", "ringo", "george")
+    println("array size is ${names.size}")
+    names[3] = "ely"
+    var ctr:Int = 0
+    while(ctr < names.size){
+        println(names[ctr])
+        ctr++
+    }
+}
