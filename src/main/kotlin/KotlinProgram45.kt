fd3c4ce0d497196526cb87f68772685ccb8dad8c
Mary Claire Capili <maryclaireosongco@gmail.com>
2023-05-05 12:40:06 +0800

    KotlinProgram40-46

---
+++ src/main/kotlin/KotlinProgram45.kt
@@ -0,0 +1,13 @@
+fun main(){
+    //Null safety
+    //null - this is the absence of a value
+    //Null Pointer Exception - NPE
+    // ? -> your variable is a nullable type
+    var name:String? = null //nullable
+
+    println ("Enter your name: ")
+    name = readln()
+
+    println ("Hello $name ")
+
+}
---
+++ src/main/kotlin/KotlinProgram46.kt
@@ -0,0 +1,20 @@
+fun main(){
+    //assertion !!
+    //nullable - non-nullable
+    val name:String? = "John"
+    val newName = name!!
+
+    println(newName)
+
+    //elvis operator
+    val name2:String? = "Peter"
+    val elvisOperatorResult = name2?: "No null values please"
+        //display name2 if not null, otherwise display "no null value please"
+    println(elvisOperatorResult)
+
+    //?.let
+    val greeting:String? = null
+    val result = greeting?.let{10+10}
+        //display let{} if not null, otherwise display null
+    println(result)
+}
